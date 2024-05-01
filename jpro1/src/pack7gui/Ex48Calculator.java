e6ce796162146540408bce62994873bf4a717080
mhlee <minhyoungi0208@gmail.com>
2024-05-01 18:28:03 +0900

    20240501 notepad, packman 만들기

--- jpro1/src/pack7gui/Ex48Calculator.java
+++ jpro1/src/pack7gui/Ex48Calculator.java
@@ -3,6 +3,8 @@ package pack7gui;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
@@ -13,164 +15,163 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
-public class Ex48Calculator extends JFrame implements ActionListener {
-	JTextField txtNum1, txtNum2;
-	ButtonGroup buttonGroup = new ButtonGroup(); // 라디오 버튼 그룹화를 위해 사용
-	JRadioButton plus, minus, cross, division;
-	JLabel lblResult;
-	JButton btn1,btn2,btn3;
-
-	public Ex48Calculator() {
+public class Ex48Calculator2 extends JFrame implements ActionListener {
+	private JTextField txt1, txt2;
+	int num1, num2;
+	private ButtonGroup rbGroup = new ButtonGroup();
+	private JRadioButton rbA, rbS, rbM, rbD;
+	private JLabel lblResult;
+	private JButton btnCalc, btnReset, btnFin;
+	
+	public Ex48Calculator2() {
 		super("미니 계산기");
-
-		comp();
-		addLayout(); 
-		setBounds(800, 200, 400, 300);
+		
+		calcLayout();
+		
+		setBounds(200, 200, 300, 400);
 		setVisible(true);
-
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 윈도우 종료
-	}
-
-	private void addLayout() {
-		btn1 = new JButton("계산");
-		btn2 = new JButton("초기화");
-		btn3 = new JButton("종료");
-		btn1.addActionListener(this);
-		btn2.addActionListener(this);
-		btn3.addActionListener(this);
-		JPanel panel = new JPanel();
-		panel.add(btn1);
-		panel.add(btn2);
-		panel.add(btn3);
 		
-		
-		add("South", panel);    // Frame은 BorderLayout이므로
+		//super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				int result = JOptionPane.showConfirmDialog(Ex48Calculator2.this, 
+						"정말 종료할거야?", "종료 확인", JOptionPane.YES_NO_OPTION);
+				if(result == JOptionPane.YES_OPTION) 
+					setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				else
+					setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+			}
+		});
 	}
 	
-	private void comp() {
-		setLayout(new GridLayout(5, 1));
-
-		JLabel lbl1 = new JLabel("숫자1 : ");
-		txtNum1 = new JTextField("", 20);
+	private void calcLayout() {
+		setLayout(new GridLayout(5,1));
+		
+		// 1행
+		JLabel lbl1 = new JLabel("숫자1: ");
+		txt1 = new JTextField("",20);
 		JPanel panel1 = new JPanel();
 		panel1.add(lbl1);
-		panel1.add(txtNum1);
-
+		panel1.add(txt1);
 		add(panel1);
-
-		JLabel lbl2 = new JLabel("숫자2 : ");
-		txtNum2 = new JTextField("", 20);
+		
+		// 2행
+		JLabel lbl2 = new JLabel("숫자2: ");
+		txt2 = new JTextField("",20);
 		JPanel panel2 = new JPanel();
 		panel2.add(lbl2);
-		panel2.add(txtNum2);
-
+		panel2.add(txt2);
 		add(panel2);
-
-		JLabel lbl3 = new JLabel("연산선택 : ");
-		plus = new JRadioButton("+", true);
-		minus = new JRadioButton("-", false);
-		cross = new JRadioButton("*", false);
-		division = new JRadioButton("/", false);
-		buttonGroup.add(plus);
-		buttonGroup.add(minus);
-		buttonGroup.add(cross);
-		buttonGroup.add(division);
-
+		
+		// 3행
+		JLabel lbl3 = new JLabel("연산선택: ");
+		rbA = new JRadioButton("+",true);
+		rbS = new JRadioButton("-",false);
+		rbM = new JRadioButton("*",false);
+		rbD = new JRadioButton("/",false);
+		rbGroup.add(rbA);
+		rbGroup.add(rbS);
+		rbGroup.add(rbM);
+		rbGroup.add(rbD);
+		
 		JPanel panel3 = new JPanel();
 		panel3.add(lbl3);
-		panel3.add(plus);
-		panel3.add(minus);
-		panel3.add(cross);
-		panel3.add(division);
-
+		panel3.add(rbA);
+		panel3.add(rbS);
+		panel3.add(rbM);
+		panel3.add(rbD);
 		add(panel3);
-
-
-		lblResult = new JLabel("결과 : ", JLabel.CENTER);
+		
+		// 4행
+		lblResult = new JLabel("결과: ", JLabel.CENTER);
 		add(lblResult);
-	}
+		
+		// 5행
+		btnCalc = new JButton("계산"); 
+		btnReset = new JButton("초기화");
+		btnFin = new JButton("종료");
+		btnCalc.addActionListener(this);
+		btnReset.addActionListener(this);
+		btnFin.addActionListener(this);
+		
+		JPanel panel5 = new JPanel();
+		panel5.add(btnCalc);
+		panel5.add(btnReset);
+		panel5.add(btnFin);
+		add(panel5);	
 
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() == btn1) {
-			if (txtNum1.getText().equals("")) {
-				JOptionPane.showMessageDialog(this, "숫자1 입력!", "알림", JOptionPane.INFORMATION_MESSAGE);
-				txtNum1.requestFocus();
-				return;
-			}
-
-			if (txtNum2.getText().equals("")) {
-				JOptionPane.showMessageDialog(this, "숫자2 입력!", "알림", JOptionPane.INFORMATION_MESSAGE);
-				txtNum2.requestFocus();
-				return;
-			}
-			
-			
-			int num1 = 0, num2 = 0;
-			try {
-				num1 = Integer.parseInt(txtNum1.getText());
-			} catch (Exception e2) {
-				JOptionPane.showMessageDialog(this, "정수 입력!", ":/", JOptionPane.INFORMATION_MESSAGE);
-				txtNum1.requestFocus();
+		if(e.getSource() == btnCalc) {			
+			// 숫자1 오류 검사
+			if(txt1.getText().equals("")) {
+				lblResult.setText("숫자1 입력해");
+				txt1.requestFocus();
 				return;
 			}
 			try {
-				num2 = Integer.parseInt(txtNum2.getText());
+				num1 = Integer.parseInt(txt1.getText());
 			} catch (Exception e2) {
-				JOptionPane.showMessageDialog(this, "정수 입력!", ":/", JOptionPane.INFORMATION_MESSAGE);
-				txtNum2.requestFocus();
+				lblResult.setText("숫자1 정수로 제대로 입력!!");
+				txt1.requestFocus();
 				return;
 			}
 			
+			// 숫자2 오류 검사
+			if(txt2.getText().equals("")) {
+				lblResult.setText("숫자2 입력해");
+				txt2.requestFocus();
+				return;
+			}
+			try {
+				num2 = Integer.parseInt(txt2.getText());
+			} catch (Exception e2) {
+				lblResult.setText("숫자2 정수로 제대로 입력!!");
+				txt2.requestFocus();
+				return;
+			}
 			
-			int result = 0;
-			if(plus.isSelected()) {
-				result = num1 + num2;
-				System.out.println(result);
-			} else if(minus.isSelected()) {
-				result = num1 - num2;
-				System.out.println(result);
-			} else if(cross.isSelected()) {
-				result = num1 * num2;
-				System.out.println(result);
-			} else {
-				if(num2 != 0) {
-					result = num1 / num2;
-				} else {
-					JOptionPane.showMessageDialog(this, "0으로 나누기 X", "알림", JOptionPane.INFORMATION_MESSAGE);
+			// 계산
+			int result;
+			if(rbA.isSelected()) lblResult.setText("결과: " + (num1 + num2));
+			else if (rbS.isSelected()) lblResult.setText("결과: " + (num1 - num2));
+			else if (rbM.isSelected()) lblResult.setText("결과: " + (num1 * num2));
+			else {
+				if(num1==0) {
+					lblResult.setText("0은 나눌 수 없어");
+					txt1.setText("");
+					txt1.requestFocus();
 				}
+				else if(num2==0) {
+					lblResult.setText("0으로 나눌 수 없어");
+					txt2.setText("");
+					txt2.requestFocus();
+				} else
+				lblResult.setText("결과: " + (double)num1 / (double)num2);
 			}
 			
-			String message = "결과 : " + result;
 			
-			lblResult.setText(message);
-		} else if(e.getSource() == btn2) {
-			if(!txtNum1.getText().equals("")) {
-				txtNum1.setText("");
-			}
-			if(!txtNum2.getText().equals("")) {
-				txtNum2.setText("");
-			}
-			if(!plus.isSelected()) {
-				plus.setSelected(true);
-			}
-				
-		} else if(e.getSource() == btn3){
-			int result = JOptionPane.showConfirmDialog(this, "종료?", "",JOptionPane.YES_NO_OPTION);
-			System.out.println(result);
-	
-			switch(result) {
-			case JOptionPane.YES_OPTION:     
-				System.exit(0);
-			case JOptionPane.NO_OPTION:
-				break;
-			}
-		}
+		} else if(e.getSource() == btnReset) {
+			txt1.setText("");
+			txt2.setText("");
+			rbA.setSelected(true);
+			rbS.setSelected(false);
+			rbM.setSelected(false);
+			rbD.setSelected(false);
+			txt1.requestFocus();
+			lblResult.setText("결과: ");
+		} else {
+			int result = JOptionPane.showConfirmDialog(this, "정말 종료할거야?", "종료 확인", JOptionPane.YES_NO_OPTION);
+			if(result == 0) System.exit(0);
+			else return;
+		}		
 	}
 
 	public static void main(String[] args) {
 		new Ex48Calculator();
-
 	}
-
 }
+
